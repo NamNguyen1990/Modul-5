bcf5f8ab103c350b7c158715ff5e5a71046db3f7
dell <namxuan.nguyen1990@gmail.com>
2022-07-19 11:07:47 +0700

    Xong phan Edit

---
+++ Ba7/QuanLySanPham/angular-product-management/src/app/product/product-edit/product-edit.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {ProductService} from "../../service/product.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
+
+@Component({
+  selector: 'app-product-edit',
+  templateUrl: './product-edit.component.html',
+  styleUrls: ['./product-edit.component.css']
+})
+export class ProductEditComponent implements OnInit {
+
+
+  productForm: FormGroup = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl(),
+    price: new FormControl(),
+    description: new FormControl(),
+
+  });
+  id: number=0;
+
+  constructor(private productService: ProductService,
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((paramMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paramMap.get('id');
+
+      const product = this.getProduct(this.id);
+      // @ts-ignore
+      this.productForm.setValue(product);
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+   getProduct(id: number) {
+    return this.productService.findById(id);
+  }
+
+  updateProduct(id: number) {
+    const product = this.productForm.value;
+    this.productService.updateProduct(id, product);
+    alert('Cập nhật thành công');
+  }
+}
